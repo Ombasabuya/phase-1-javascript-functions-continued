c805b7c12193e33aff71d75e59fcb5797aac7465
ombasaabuya <ombasabuya@gmail.com>
2022-12-01 08:50:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity){
+  let value = activity || "roller-skate"
+
+  return "This Saturday, I want to " + value +"!";
+
+}
+
+const mondayWork = function(event){
+  let var1 = event || "go to the office"
+
+  return "This Monday, I will " + var1 + "."
+
+}
+function wrapAdjective(visualFlair){
+
+  return function(statement){
+
+      let finalStatement = statement || "special";
+
+      return `You are ${visualFlair}${finalStatement}${visualFlair}!`;
+}
+}
